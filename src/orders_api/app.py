9f9a0b560598c1d1ee91aab3a3700a5884d12898
Patrick Muehlbauer <patrick@opentrash.org>
2021-01-05 05:53:50 +0100

    VSCode <3 Docker

--- src/orders_api/app.py
+++ src/orders_api/app.py
@@ -10,6 +10,10 @@ def create_app() -> FastAPI:
 
     @app.get("/health")
     async def health() -> str:
+        import debugpy
+        debugpy.listen(("0.0.0.0", 5678))
+        debugpy.wait_for_client()
+        breakpoint()
         return "ok"
 
     return app
